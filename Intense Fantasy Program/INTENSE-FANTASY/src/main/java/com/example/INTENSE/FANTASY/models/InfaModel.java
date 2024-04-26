64bca7a0e59d3cf5839749186e36a321a48ecc41
Fakaa Uwu <you@example.com>
2024-04-26 08:08:28 -0600

    Nueva intense fantasy

--- Intense Fantasy Program/INTENSE-FANTASY/src/main/java/com/example/INTENSE/FANTASY/models/InfaModel.java
+++ Intense Fantasy Program/INTENSE-FANTASY/src/main/java/com/example/INTENSE/FANTASY/models/InfaModel.java
@@ -1,2 +1,73 @@
-package com.example.INTENSE.FANTASY.models;public class InfaModel {
+package com.example.INTENSE.FANTASY.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+
+public class InfaModel {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,unique = true)
+
+    private Long id;
+
+    private Integer code;
+
+    private String name;
+
+    private String email;
+
+    private String degree;
+
+    private Double Score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public Double getScore() {
+        return Score;
+    }
+
+    public void setScore(Double score) {
+        Score = score;
+    }
 }
