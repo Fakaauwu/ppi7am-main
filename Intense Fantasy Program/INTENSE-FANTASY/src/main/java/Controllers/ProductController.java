011627ae40e531d440f324ce0ed9f24d54b00b87
Fakaauwu <luisfaa@gmail.com>
2024-05-02 21:25:16 -0600

    Commit de Diego

--- Intense Fantasy Program/INTENSE-FANTASY/src/main/java/Controllers/ProductController.java
+++ Intense Fantasy Program/INTENSE-FANTASY/src/main/java/Controllers/ProductController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     ProductServices productService;
